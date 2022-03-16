522c421482c3371b5132013360c4920949c4414a
Deedeo <daradeo4u@gmail.com>
2022-03-16 02:15:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Description: Uses the main header file, that brings in the _putchar
+ * function
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch 'a';
+
+	while (ch <= 'z')
+{
+	_putchar(ch);
+	++ch;
+}
+	_putchar('\n');
+}
