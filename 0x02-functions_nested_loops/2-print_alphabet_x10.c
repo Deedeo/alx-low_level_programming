f6d68efae78302f34379ffbc33f465ba4d86b1ff
Deedeo <daradeo4u@gmail.com>
2022-03-16 03:29:11 -0700

    2bit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (counter++ <= 9)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
