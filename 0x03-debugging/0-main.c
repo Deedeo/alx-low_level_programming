eaa5862790ea127f1ecae89aa17a07d6d596d53c
Deedeo <daradeo4u@gmail.com>
2022-03-17 04:00:40 -0700

    bug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests finction that proints if interger is positive or negative
+ * return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
