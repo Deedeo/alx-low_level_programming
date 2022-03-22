71b4f5947a40fad8dd513ea62bb91755f219cfde
Deedeo <daradeo4u@gmail.com>
2022-03-22 14:06:49 -0700

    gcc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * print_rev - print reverse
- * @s: input string
- *
- * Description:  prints a string, in reverse
- * Return: Always (0)
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
  */
-
 void print_rev(char *s)
 {
-	int left, len, i;
+	int fcounter = 0;
+	int i, n;
 
-	len = 0;
-	left = 0;
-
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		len = i;
+		fcounter++;
 	}
-	for (; len >= left; len--)
+
+	for (n = (fcounter - 1); n >= 0; n--)
 	{
-		_putchar(s[len]);
+		_putchar(s[n]);
 	}
 	_putchar('\n');
 }
