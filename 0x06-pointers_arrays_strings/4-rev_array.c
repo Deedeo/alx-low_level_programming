6a46418c9d92e12cd969c14ae796c25129fb4f27
Deedeo <daradeo4u@gmail.com>
2022-03-25 02:04:07 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse
+ * @a : pointer to int params
+ * @n : pointer to int params
+ * Return: no
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+int tmp;
+
+i = 0;
+j = n - 1;
+
+while (i < j)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+i++;
+j--;
+}
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"4-rev_array.c" [New File]                                                          0,0-1         All
+
